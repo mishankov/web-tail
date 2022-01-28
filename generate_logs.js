fda05301ebad9c0c47da79ff11ffa29b72424cc4
mishankov <mishankov@mail.com>
2022-01-28 20:30:48 +0300

    misc

--- generate_logs.js
+++ generate_logs.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 async function run()  {
     while (true) {
-        await new Promise(r => setTimeout(r, 500));
+        await new Promise(r => setTimeout(r, 1000));
     
         let logLine = `[${new Date()}] [MyApp] [INFO] - ${Math.random()}\n`
     
