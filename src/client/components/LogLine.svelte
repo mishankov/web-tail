caa72f35e4cc02f9c14c80384128779bc91ff7aa
mishankov <mishankov@mail.com>
2022-01-30 23:34:19 +0300

    good take on #11. needs refactoring

--- src/client/components/LogLine.svelte
+++ src/client/components/LogLine.svelte
@@ -4,31 +4,32 @@
     export let line: string;
     export let selectLine: string;
 
-    let splittedLine: Array<string>;
+    let selectRegex: RegExp;
+
+    function escapeRegExp(string) {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    }
+    
     $: {
-        if (selectLine.length > 0) {
-            if ($regexFilter){
-                try {
-                splittedLine = line.split(new RegExp(selectLine, 'g'));
-                } catch {
-                    splittedLine = [line];
-                }
-            } else {
-                splittedLine = line.split(selectLine);
+        if ($regexFilter) {
+            try {
+                selectRegex = new RegExp(selectLine, "g");
+            } catch {
+                selectRegex = new RegExp(escapeRegExp(selectLine), "g");
             }
-            
-        } else {
-            splittedLine = [line];
         }
     }
+    
     
 </script>
 
 <div class="line">
-    {#if selectLine !== undefined && selectLine.length > 0}
-        {#each splittedLine as linePart, i(i)}
-            <span>{linePart}</span>{#if i !== splittedLine.length - 1 }<span class="selected-log-line">{selectLine}</span>{/if}
-        {/each}
+    {#if selectLine.length > 0}
+        {#if $regexFilter}
+            <span>{@html line.replaceAll(selectRegex, '<span class="selected-log-line">$&</span>')}</span>
+        {:else}
+            <span>{@html line.replaceAll(selectLine, '<span class="selected-log-line">$&</span>')}</span>
+        {/if}
     {:else}
         <span>{line}</span>
     {/if}
@@ -43,7 +44,7 @@
         border-bottom: 1px solid var(--color-dark-100);
     }
 
-    .selected-log-line {
+    :global(.selected-log-line) {
         background-color: var(--color-accent-100);
         color: var(--color-dark-100);
         padding: 2px;
--- src/client/components/LogLines.svelte
+++ src/client/components/LogLines.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
     import LogLine from "./LogLine.svelte";
     import { logs } from "../stores/logs";
-    import { filterLogs, reverseLogs } from "../stores/settings";
+    import { filterLogs, reverseLogs, regexFilter } from "../stores/settings";
 
     export let searchString: string;
 
     let filteredLogs: {id: string, item: string}[] = [];
+    let selectRegex: RegExp;
+
+    function escapeRegExp(string) {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    }
 
     $: {
         filteredLogs = $logs.toArray().filter((log) => {
-            return !$filterLogs || $filterLogs && log.item.includes(searchString)
+            if ($regexFilter) {
+                try {
+                    selectRegex = new RegExp(searchString, "g");
+                } catch {
+                    selectRegex = new RegExp(escapeRegExp(searchString), "g");
+                }
+
+                return !$filterLogs || $filterLogs && log.item.match(selectRegex) !== null
+            } else {
+                return !$filterLogs || $filterLogs && log.item.includes(searchString)
+            }
         });
 
         if ($reverseLogs) filteredLogs.reverse();
