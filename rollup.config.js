ff1b6a58d6aed2f89f185501035633606a8f5a08
mishankov <mishankov@mail.com>
2022-02-01 15:44:12 +0300

    remove unused serve()

--- rollup.config.js
+++ rollup.config.js
@@ -10,31 +10,6 @@ import css from "rollup-plugin-css-only";
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-  let server;
-
-  function toExit() {
-    if (server) server.kill(0);
-  }
-
-  return {
-    writeBundle() {
-      if (server) return;
-      server = require("child_process").spawn(
-        "npm",
-        ["run", "start", "--", "--dev"],
-        {
-          stdio: ["ignore", "inherit", "inherit"],
-          shell: true,
-        }
-      );
-
-      process.on("SIGTERM", toExit);
-      process.on("exit", toExit);
-    },
-  };
-}
-
 export default [
   {
     input: ["src/server/index.ts"],
