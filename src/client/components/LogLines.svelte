b1633b2258fd0a36f170202bbfdba98b3df41f32
Denis Mishankov <mishankov@mail.com>
2023-04-01 14:13:38 +0300

    add sticky scroll

--- src/client/components/LogLines.svelte
+++ src/client/components/LogLines.svelte
@@ -1,55 +1,66 @@
 <script lang="ts">
-    import LogLine from "./LogLine.svelte";
-    import { escapeRegExp } from "../utils";
-    import { logs } from "../stores/logs";
-    import { filterLogs, reverseLogs, regexFilter, caseSensitive } from "../stores/settings";
+  import LogLine from "./LogLine.svelte";
+  import { escapeRegExp } from "../utils";
+  import { logs } from "../stores/logs";
+  import {
+    filterLogs,
+    reverseLogs,
+    regexFilter,
+    caseSensitive,
+  } from "../stores/settings";
 
-    export let searchString: string;
+  export let searchString: string;
 
-    let filteredLogs: {id: string, item: string}[] = [];
-    let selectRegex: RegExp;
+  let filteredLogs: { id: string; item: string }[] = [];
+  let selectRegex: RegExp;
 
-    $: {
-        filteredLogs = $logs.toArray().filter((log) => {
-            let regexFlags = "gi";
+  $: {
+    filteredLogs = $logs.toArray().filter((log) => {
+      let regexFlags = "gi";
 
-            if ($caseSensitive) regexFlags = "g";
+      if ($caseSensitive) regexFlags = "g";
 
-            if ($regexFilter) {
-                try {
-                    selectRegex = new RegExp(searchString, regexFlags);
-                } catch {
-                    selectRegex = new RegExp(escapeRegExp(searchString), regexFlags);
-                }
-            } else {
-                selectRegex = new RegExp(escapeRegExp(searchString), regexFlags);
-            }
+      if ($regexFilter) {
+        try {
+          selectRegex = new RegExp(searchString, regexFlags);
+        } catch {
+          selectRegex = new RegExp(escapeRegExp(searchString), regexFlags);
+        }
+      } else {
+        selectRegex = new RegExp(escapeRegExp(searchString), regexFlags);
+      }
 
-            return !$filterLogs || $filterLogs && log.item.match(selectRegex) !== null
-        });
+      return (
+        !$filterLogs || ($filterLogs && log.item.match(selectRegex) !== null)
+      );
+    });
 
-        if ($reverseLogs) filteredLogs.reverse();
-    }
-    
+    if ($reverseLogs) filteredLogs.reverse();
+  }
 </script>
 
-
 <div class="logs">
-    {#each filteredLogs as logLine(logLine.id)}
-        <LogLine line={logLine.item} selectRegex={selectRegex}/>
-    {/each}
+  {#each filteredLogs as logLine (logLine.id)}
+    <LogLine line={logLine.item} {selectRegex} />
+  {/each}
+  <div class="scroll-anchor" />
 </div>
 
 <style>
-    .logs {
-        position: fixed;
-        left: 0;
-        top: 2.8rem;
-        min-height: calc(100vh - 2.8rem);
-        width: 100%;
-        max-height: calc(100vh - 2.8rem);
-        background-color: var(--color-dark-80);
-        overflow: auto;
-        overflow-wrap: break-word;
-    }
+  .logs {
+    position: fixed;
+    left: 0;
+    top: 2.8rem;
+    min-height: calc(100vh - 2.8rem);
+    width: 100%;
+    max-height: calc(100vh - 2.8rem);
+    background-color: var(--color-dark-80);
+    overflow: auto;
+    overflow-wrap: break-word;
+  }
+
+  .scroll-anchor {
+    overflow-anchor: auto;
+    height: 1px;
+  }
 </style>
