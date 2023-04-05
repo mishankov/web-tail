ccef18f91c5ef40a24debbe57ac4de252c1598db
Denis Mishankov <mishankov@mail.com>
2023-04-05 21:55:31 +0300

    Updates (#42)
    * add sticky scroll
    
    * Bump qs and express (#41)
    
    Bumps [qs](https://github.com/ljharb/qs) to 6.11.0 and updates ancestor dependency [express](https://github.com/expressjs/express). These dependencies need to be updated together.
    
    
    Updates `qs` from 6.9.6 to 6.11.0
    - [Release notes](https://github.com/ljharb/qs/releases)
    - [Changelog](https://github.com/ljharb/qs/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/ljharb/qs/compare/v6.9.6...v6.11.0)
    
    Updates `express` from 4.17.2 to 4.18.2
    - [Release notes](https://github.com/expressjs/express/releases)
    - [Changelog](https://github.com/expressjs/express/blob/master/History.md)
    - [Commits](https://github.com/expressjs/express/compare/4.17.2...4.18.2)
    
    ---
    updated-dependencies:
    - dependency-name: qs
      dependency-type: indirect
    - dependency-name: express
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix node version for docker
    
    * upload execs
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/client/components/LogLine.svelte
+++ src/client/components/LogLine.svelte
@@ -1,31 +1,36 @@
 <script lang="ts">
-    export let line: string;
-    export let selectRegex: RegExp;
+  export let line: string;
+  export let selectRegex: RegExp;
 </script>
 
 <div class="line">
-    {#if "".match(selectRegex) === null}
-        <span>{@html line.replaceAll(selectRegex, '<span class="selected-log-line">$&</span>')}</span>
-    {:else}
-        <span>{line}</span>
-    {/if}
+  {#if "".match(selectRegex) === null}
+    <span
+      >{@html line.replaceAll(
+        selectRegex,
+        '<span class="selected-log-line">$&</span>'
+      )}</span
+    >
+  {:else}
+    <span>{line}</span>
+  {/if}
 </div>
 
-
 <style>
-    .line {
-        font-family: monospace;
-        font-size: 1rem;
-        padding: .3em;
-        color: var(--color-light-100);
-        border-bottom: 1px solid var(--color-dark-100);
-    }
+  .line {
+    font-family: monospace;
+    font-size: 1rem;
+    padding: 0.3em;
+    color: var(--color-light-100);
+    border-bottom: 1px solid var(--color-dark-100);
+    overflow-anchor: none;
+  }
 
-    :global(.selected-log-line) {
-        background-color: var(--color-accent-100);
-        color: var(--color-dark-100);
-        padding: 2px;
-        border: 1px solid var(--color-dark-100);
-        border-radius: 10px;
-    }
+  :global(.selected-log-line) {
+    background-color: var(--color-accent-100);
+    color: var(--color-dark-100);
+    padding: 2px;
+    border: 1px solid var(--color-dark-100);
+    border-radius: 10px;
+  }
 </style>
--- src/client/pages/Logs.svelte
+++ src/client/pages/Logs.svelte
@@ -1,58 +1,56 @@
 <script lang="ts">
-    import Header from "../components/Header.svelte";
-    import LogLines from "../components/LogLines.svelte";
-    import { logs } from "../stores/logs";
-    import { logWindow } from "../stores/settings";
-    import { CircularBuffer } from "../types/CircularBuffer";
+  import Header from "../components/Header.svelte";
+  import LogLines from "../components/LogLines.svelte";
+  import { logs } from "../stores/logs";
+  import { logWindow } from "../stores/settings";
+  import { CircularBuffer } from "../types/CircularBuffer";
 
-    let searchString = "";
-    let source = "";
-    let socket: WebSocket;
+  let searchString = "";
+  let source = "";
+  let socket: WebSocket;
 
-    $: {
-        if (socket !== undefined) {
-            socket.close();
+  $: {
+    if (socket !== undefined) {
+      socket.close();
+    }
+
+    if (source !== "") {
+      logs.update((buff) => {
+        return new CircularBuffer<string>($logWindow);
+      });
+      socket = new WebSocket(`ws://${location.host}/${source}/${$logWindow}`);
+
+      socket.addEventListener("open", function (event) {
+        console.log("Socket opened", source);
+      });
+
+      socket.addEventListener("message", function (event) {
+        if (event.data.length > 0) {
+          logs.update((buff) => {
+            for (let line of event.data.split("\n")) {
+              if (line.length > 0) buff.push(line);
+            }
+            return buff;
+          });
         }
-
-        if (source !== "") {
-            logs.update(buff => {return new CircularBuffer<string>($logWindow)});
-            socket = new WebSocket(`ws://${location.host}/${source}/${$logWindow}`);
-
-            socket.addEventListener("open", function (event) {
-                console.log('Socket opened', source);
-            });
-
-            socket.addEventListener("message", function (event) {
-                if (event.data.length > 0) {
-                    logs.update(buff =>  {
-                        for (let line of event.data.split("\n")) {
-                            if (line.length > 0) buff.push(line);   
-                        }
-                        return buff;                   
-                    });
-                }
-            });
-        }
+      });
+    }
     // }
 
     // $: {
-        logs.update(buff => {
-            buff.setLength($logWindow);
-            return buff;
-        })
-    }
+    logs.update((buff) => {
+      buff.setLength($logWindow);
+      return buff;
+    });
+  }
 </script>
 
 <svelte:head>
-    <title>Web tail | {source}</title>
+  <title>Web tail | {source}</title>
 </svelte:head>
 
-<Header bind:source={source} bind:searchString={searchString}/>
+<Header bind:source bind:searchString />
 
 <main>
-    <LogLines searchString={searchString}/>    
+  <LogLines {searchString} />
 </main>
-
-<style>
-	
-</style>
