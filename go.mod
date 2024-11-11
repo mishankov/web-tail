0158eeb65fc9677df1974e11e46ac60299fd2f6d
Denis Mishankov <mishankov@mail.com>
2024-11-11 19:17:19 +0300

    getting config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-tail
 
 go 1.23.0
+
+require github.com/BurntSushi/toml v1.4.0
--- server/main.go
+++ server/main.go
@@ -1,1 +1,11 @@
 package main
+
+import "log"
+
+func main() {
+	config, err := getConfig()
+	if err != nil {
+		log.Fatalln("Error getting config:", err)
+	}
+	log.Println(config)
+}
