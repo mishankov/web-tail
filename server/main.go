b42b2f5a0684fb9fae773ef5dc42c7f8d81cc7c0
Denis Mishankov <mishankov@mail.com>
2024-11-13 08:20:06 +0300

    add chi and stuff

--- server/main.go
+++ server/main.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 	"time"
 	"web-tail/pkg/logging"
+
+	"github.com/go-chi/chi/v5"
 )
 
 var logger = logging.NewLogger("handlers")
@@ -27,10 +29,23 @@ func handleSources(w http.ResponseWriter, req *http.Request) {
 		logger.Error("Error marshaling sources to JSON:", err)
 	}
 
+	w.Header().Add("Content-Type", "application/json")
+
 	_, err = w.Write(sourcesJson)
 	if err != nil {
 		logger.Error("Error writing response for sources:", err)
 	}
+}
+
+func router() chi.Router {
+	r := chi.NewRouter()
+
+	r.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "Time: %v", time.Now()) })
+	r.Get("/sources", handleSources)
+
+	r.Handle("/*", http.FileServer(http.Dir("public")))
+
+	return r
 }
 
 func main() {
@@ -40,13 +55,11 @@ func main() {
 	}
 	logger.Infof("Config loaded. Sources amount: %v. Severs amount: %v", len(config.Sources), len(config.Servers))
 
-	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "Time: %v", time.Now()) })
-	http.HandleFunc("/sources", handleSources)
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	r := router()
 
 	logger.Infof("Starting server: http://localhost:%v", config.Port)
 
-	if err := http.ListenAndServe(":"+strconv.Itoa(int(config.Port)), nil); err != nil {
+	if err := http.ListenAndServe(":"+strconv.Itoa(int(config.Port)), r); err != nil {
 		logger.Fatal("Can't start server:", err)
 	}
 }
