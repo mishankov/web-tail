d9d10739c9c9793c8e663f028d4f4909b6492247
Denis Mishankov <mishankov@mail.com>
2024-11-15 08:59:24 +0300

    ssh:docker works for some reason

--- go.mod
+++ go.mod
@@ -10,10 +10,11 @@ require (
 	github.com/gorilla/websocket v1.5.3
 	github.com/mishankov/go-utlz v0.0.3
 	github.com/nxadm/tail v1.4.11
+	golang.org/x/crypto v0.29.0
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+	golang.org/x/sys v0.27.0 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- server/sources.go
+++ server/sources.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"iter"
 	"os/exec"
 	"strconv"
 
 	"github.com/nxadm/tail"
+	"golang.org/x/crypto/ssh"
 )
 
 type Tailer interface {
 	Tail() (iter.Seq[string], error)
 }
 
+// TODO: refactor to Tail take initial amount arg
 type BaseTailer struct {
 	initialAmount int
 }
@@ -24,6 +27,8 @@ func NewTailerFromSource(source SourceConfig, initialAmount int) Tailer {
 		return LocalFile{filePath: source.Path, BaseTailer: BaseTailer{initialAmount: initialAmount}}
 	case "local:docker":
 		return LocalDocker{containerId: source.ContainerId, BaseTailer: BaseTailer{initialAmount: initialAmount}}
+	case "ssh:docker":
+		return Remote{host: source.Host, port: source.Port, username: source.Username, password: source.Password, privateKeyPath: source.PrivateKeyPath, command: fmt.Sprintf("docker logs -f -n %v %v", strconv.Itoa(initialAmount), source.ContainerId), BaseTailer: BaseTailer{initialAmount: initialAmount}}
 	default:
 		return nil
 	}
@@ -76,5 +81,63 @@ func (ld LocalDocker) Tail() (iter.Seq[string], error) {
 		}
 
 		logger.Infof("Container %v stopped", ld.containerId)
+	}, nil
+}
+
+type Remote struct {
+	BaseTailer
+	host           string
+	port           port
+	username       string
+	password       string
+	privateKeyPath string
+	command        string
+}
+
+func (r Remote) hostAndPort() string {
+	return fmt.Sprintf("%v:%v", r.host, r.port)
+}
+
+func (r Remote) Tail() (iter.Seq[string], error) {
+	logger.Debug("Start of Tail. Command:", r.command)
+
+	sshConfig := &ssh.ClientConfig{
+		User: r.username,
+		Auth: []ssh.AuthMethod{ssh.Password(r.password)}, //TODO: implement auth with key
+	}
+	sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+
+	client, err := ssh.Dial("tcp", r.hostAndPort(), sshConfig)
+	if err != nil {
+		logger.Errorf("Error connectig to %v: %v", r.hostAndPort(), err)
+		return nil, err
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		logger.Error("Error creating new session:", err)
+		return nil, err
+	}
+
+	stdout, err := session.StdoutPipe()
+	if err != nil {
+		logger.Error("Error getting stdout:", err)
+	}
+
+	return func(yield func(string) bool) {
+		logger.Debug("Begin of yield")
+
+		scanner := bufio.NewScanner(stdout)
+		logger.Debug("Scanner created")
+
+		if err := session.Start(r.command); err != nil {
+			logger.Error("Error running command:", err)
+		}
+
+		for scanner.Scan() {
+			yield(scanner.Text())
+		}
+
+		logger.Debug("End of yield")
 	}, nil
 }
